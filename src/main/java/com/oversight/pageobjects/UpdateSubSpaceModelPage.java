115d2b05f0c4a1aaa9c6280d0eac823f621a0e75
Amali <amalisashini@gmail.com>
2024-02-22 09:21:51 +0530

    sub space update script

--- src/main/java/com/oversight/pageobjects/UpdateSubSpaceModelPage.java
+++ src/main/java/com/oversight/pageobjects/UpdateSubSpaceModelPage.java
@@ -1,2 +1,34 @@
-package com.oversight.pageobjects;public class UpdateSubSpaceModelPage {
+package com.oversight.pageobjects;
+
+import com.oversight.actiondriver.Action;
+import com.oversight.base.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class UpdateSubSpaceModelPage extends BaseClass {
+
+    Action action = new Action();
+    public UpdateSubSpaceModelPage() {
+
+        PageFactory.initElements(driver,this);
+
+    }
+
+    @FindBy(xpath="//div[@class='d-block']//input[@name='spaceClusterLabel']")
+    WebElement billingSpaceNameField;
+
+    @FindBy(xpath="//div[@class='d-block']//button[@class='undefined undefined app-button_app-button__xBnO8 app-button_app-button-blue__MyFQW app-button_app-button-medium__WWpFr false']")
+    WebElement confirmBtn;
+
+    public DevicePage updateNewBillingSpace(String billingSpaceName){
+
+        action.clearTextField(driver, (By) billingSpaceNameField);
+        action.type(billingSpaceNameField, billingSpaceName);
+        action.click(driver, confirmBtn);
+        return new DevicePage();
+
+    }
+
 }
